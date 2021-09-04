67d3cb339f49d6b15d7f83a4f8e75e66a5c583fd
Ebuka Egbunam <51460167+eegbunam@users.noreply.github.com>
2021-09-03 19:58:59 -0500

    adding comments

--- python_stripe_server /server.py
+++ python_stripe_server /server.py
@@ -5,19 +5,16 @@ import stripe
 from decouple import config
 
 
-stripe.api_key = config('STRIPE_API_KEY')
-
+stripe.api_key = config('STRIPE_API_KEY') # create a .env file and add your key into that file. Name your key 'STRIPE_API_KEY'
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
-    return "<h1>Hello, World Stripe</h1>"
-
-
-
 @app.route('/create-payment-intent', methods=['POST','GET'])
 def create_payment():
+    '''
+    This function creates the payment intent using stripe's api and returns a client secret
+    that can be used any client
+    '''
     try:
         data = json.loads(request.data)
         print(data)
@@ -31,10 +28,6 @@ def create_payment():
         })
     except Exception as e:
         return jsonify(error=str(e)), 403
-
-@app.route('/test') #Automatically makes it a GET request if no Method parameters are passed
-def test_Method():
-    return "<h1>This is a test Method to check the health of my Flask server </h1>"
 
 if  __name__ == "__main__":
     app.run(debug=True , use_reloader = False)
